b19d68ffac940be9dbf62196d95e32c6deca573b
GermainMichaud <gmichaud.dev@gmail.com>
2021-08-30 20:12:57 +0200

    Create Card component

---
+++ src/components/shared/Card.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0.7rem;
+  border-radius: 0.7rem;
+  background: #fff;
+  box-shadow: 0 0 5px rgba(50, 50, 50, 0.3);
+`
