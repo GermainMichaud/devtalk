b4e1aa007153364b582cf8b072175552c0b9577e
GermainMichaud <gmichaud.dev@gmail.com>
2021-08-30 20:41:44 +0200

    Add link to Talk card

--- src/components/Talk.jsx
+++ src/components/Talk.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Paragraph, Row, Title } from './shared'
 import { Card } from './shared/Card'
 
 const Talk = ({ talk }) => {
   return (
-    <Card>
-      <Title>{talk.title}</Title>
-      <Paragraph pv="0" ph="0" textLeft>
-        {talk.description}
-      </Paragraph>
-    </Card>
+    <Link to={`/talk/${talk._id}`}>
+      <Card>
+        <Title>{talk.title}</Title>
+        <Paragraph pv="0" ph="0" textLeft>
+          {talk.description}
+        </Paragraph>
+      </Card>
+    </Link>
   )
 }
 
