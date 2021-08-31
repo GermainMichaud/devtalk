626f2bc543083f77bec3d851aad55368a54fc4a8
GermainMichaud <gmichaud.dev@gmail.com>
2021-08-31 12:47:36 +0200

    Update ProtectedRoute component

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -6,9 +6,12 @@ import userStore from '../store/userStore'
 const ProtectedRoute = ({ component: Component, ...rest }) => {
   const [user, setUser] = useState({})
   const setUserStore = userStore(state => state.setUser)
+  const userLoading = userStore(state => state.loading)
+  const setUserLoading = userStore(state => state.setLoading)
 
   useEffect(() => {
     const getUser = async () => {
+      setUserLoading(true)
       try {
         const { data } = await userAPI.get()
         setUser(data)
@@ -16,11 +19,15 @@ const ProtectedRoute = ({ component: Component, ...rest }) => {
       } catch (error) {
         setUser(null)
         setUserStore(null)
+      } finally {
+        setUserLoading(false)
       }
     }
     getUser()
     return () => {}
   }, [])
+
+  if (userLoading) return <h1>Loading...</h1>
 
   return (
     <Route
